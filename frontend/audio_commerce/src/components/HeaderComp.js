39d8db1a29eb31f513fe653e436bd79d2260be61
KurtSchwimmbacher <231002@virtualwindow.co.za>
2024-05-20 21:55:55 +0200

    add animation to header comp

--- frontend/audio_commerce/src/components/HeaderComp.js
+++ frontend/audio_commerce/src/components/HeaderComp.js
@@ -1,60 +1,64 @@
 import homeVid from "../assets/videos/Home_pg_vid.mp4";
 import productsImg from "../assets/images/speaker-img.png";
 import tempImg from "../assets/images/earbud-img.jpg";
-import headphoneTemp from "../assets/images/headphone-2.png"
+import headphoneTemp from "../assets/images/headphone-2.png";
 
 import { useEffect, useState } from "react";
+import "../styles/HeaderComp.css"; 
 
 function HeaderComp(props) {
-
   const [isImg, setIsImg] = useState(false);
   const [imgSrc, setImgSrc] = useState("");
-  const [imgStyle,setImgStyle] = useState({});
+  const [imgStyle, setImgStyle] = useState({});
+  const [isLoaded, setIsLoaded] = useState(false); // State to track if the component has loaded
 
   useEffect(() => {
     switch (props.where) {
       case 'Home':
         setImgSrc(homeVid);
         setIsImg(false);
-      break;
-
+        break;
       case 'Products':
         setImgSrc(productsImg);
         setIsImg(true);
-      break;
-
+        break;
       case 'SingleProduct':
         // replace with img src from database
         setImgSrc(headphoneTemp);
         setIsImg(true);
-        setImgStyle({transform: 'rotate(20)'})
-      break;
-
+        setImgStyle({ transform: 'rotate(20deg)' });
+        break;
       case 'Community':
         // replace with img src from database
         setImgSrc(tempImg);
         setIsImg(true);
-      break;
-
+        break;
       default:
         break;
     }
-  },[]);
-   
-    return(
-      <>
-        {isImg ? (
-          <img src={imgSrc} style={imgStyle} className="video-home-pg"></img>
-         ) : (
-          <video className="video-home-pg" src={imgSrc} autoPlay loop muted> </video>
-        )}
-      </>
-      // {isImg ? (
-      //     <img src={imgSrc} className="video-home-pg"></img>
-      // ) : (
-      //   <video className="video-home-pg" src={imgSrc} autoPlay loop muted> </video>
-      // )}
-    );
+
+    setIsLoaded(true); // Set the component as loaded
+  }, [props.where]);
+
+  return (
+    <>
+      {isImg ? (
+        <img
+          src={imgSrc}
+          style={imgStyle}
+          className={`media ${isLoaded ? 'scale-up' : ''}`} // Add fade-in class if loaded
+        />
+      ) : (
+        <video
+          className={`media ${isLoaded ? 'scale-up' : ''}`} // Add fade-in class if loaded
+          src={imgSrc}
+          autoPlay
+          loop
+          muted
+        />
+      )}
+    </>
+  );
 }
 
-export default HeaderComp; 
+export default HeaderComp;
